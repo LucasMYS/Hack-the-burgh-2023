ecff920f018daeff76c333d2ea7c5c6a8a9afff6
Sbzro12345 <72307706+Sbzro12345@users.noreply.github.com>
2023-03-08 14:49:36 +0000

    Made by Luca (luck58) on 5 March 2023, 9.10am

---
+++ backups/dump.py
@@ -0,0 +1,65 @@
+#Dumps all positions, execute in last 30s of competition if net position is positive
+
+'''
+Contains reset_position function
+PLEASE DON'T CHANGE
+Will call reset_position on "green" if run directly
+'''
+from typing import List
+from optibook import common_types as t
+from optibook import ORDER_TYPE_IOC, ORDER_TYPE_LIMIT, SIDE_ASK, SIDE_BID
+from optibook.exchange_responses import InsertOrderResponse
+from optibook.synchronous_client import Exchange
+from time import sleep
+
+
+def reset_positions(e, instruments="green", verbose=False):
+    if instruments == "green":
+        INSTRUMENTS_ID = ["C2_GREEN_ENERGY_ETF", "C2_SOLAR_CO", "C2_WIND_LTD"]
+    elif instruments == "fossil":
+        INSTRUMENTS_ID = ["C1_FOSSIL_FUEL_ETF", "C1_GAS_INC", "C1_OIL_CORP"]
+    else:
+        print(f"Please enter a valid instrument. You entered '{instruments}'")
+        return
+    for INSTRUMENT_ID in INSTRUMENTS_ID:
+        positions = e.get_positions()
+        while positions[INSTRUMENT_ID] != 0:
+
+            positions = e.get_positions()
+            if verbose:
+                print(positions)
+
+            if positions[INSTRUMENT_ID] < 0:
+                BUY = True
+            elif positions[INSTRUMENT_ID] > 0:
+                BUY = False
+            else:
+                continue
+
+            if BUY:
+                SIDE = SIDE_BID
+                price = e.get_last_price_book(INSTRUMENT_ID).asks[0].price
+            else:
+                SIDE = SIDE_ASK
+                price = e.get_last_price_book(INSTRUMENT_ID).bids[0].price
+
+            volume = abs(positions[INSTRUMENT_ID])
+            if verbose:
+                print("Volume: ", volume)
+            bid_response: InsertOrderResponse = e.insert_order(INSTRUMENT_ID, price=price, volume=volume, side=SIDE, order_type=ORDER_TYPE_IOC)
+            if verbose:
+                print(bid_response)
+            sleep(1)
+            if verbose:
+                print("Position: ", positions[INSTRUMENT_ID])
+            
+        else:
+            if verbose:
+                print(f"Position of '{INSTRUMENT_ID}' is 0")
+    
+
+if __name__ == '__main__':
+    e = Exchange()
+    e.connect()
+    reset_positions(e, "green")
+    reset_positions(e, "fossil")
