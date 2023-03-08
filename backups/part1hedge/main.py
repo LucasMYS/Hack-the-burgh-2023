cfba12be721b911f1f212ee5cd27d0cf8767b4b7
Sbzro12345 <72307706+Sbzro12345@users.noreply.github.com>
2023-03-08 17:00:29 +0000

    Made by Luca (luck58), Aurora, Lucas and Siren on 5 March 2023, 9.10am

---
+++ backups/part1hedge/main.py
@@ -0,0 +1,30 @@
+import time
+from optibook.synchronous_client import Exchange
+import part1_refactored as part1_refactored
+import part1_refactored1 as part1_refactored1
+import reset_positions
+
+def main():
+    exchange = Exchange()
+    exchange.connect()
+
+    sleep_duration_sec = 0.2
+    reset_positions.reset_positions(exchange, verbose=True)
+    initial_pnl = exchange.get_pnl()
+    
+    while True:
+        #part1fossil.trade_cycle(exchange)
+        #part1green.trade_cycle(exchange)
+        #part2.trade_cycle(exchange)
+        part1_refactored.trade_cycle(exchange,False)
+        part1_refactored1.trade_cycle(exchange,False)
+        print("Positions: ", end="")
+        print(exchange.get_positions())
+        print("Delta PnL: ",  exchange.get_pnl() - initial_pnl)
+        print()
+        print("----------------------------")
+        print()
+        time.sleep(sleep_duration_sec)
+
+if __name__ == '__main__':
+    main()
